6013a301ff886ba8022f053029991e71ec0be256
Samar Omar <samaromm98@gmail.com>
2020-02-14 19:57:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ class Polygon{
   constructor(sides){
     this.sides=sides
   }
-  get countSides(){return sides.length}
-  get 
+  get countSides(){return this.sides.length}
+  get perimeter(){
+    let sum=0
+    for(let i of this.sides){
+      sum+=i
+    }
+    
+  }
 }
